febbafa45fd3025cf234b038996b721c8c3158e7
jessec1001 <jessecorralejr@gmail.com>
2020-04-30 02:22:54 +0200

    fix unicode issue

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,25 @@ import "reflect-metadata";
 import { config } from "dotenv";
 config();
 import debug from "debug";
-import { createConnection } from "typeorm";
+import { createConnection, getConnectionOptions } from "typeorm";
 import Application from "./services/Application";
 import SocketIOServer from "./services/SocketIOServer";
+import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOptions";
 
-createConnection().then(() => {
+async function init(): Promise<void> {
+  try {
+    const connectionOptions = await getConnectionOptions();
+    await createConnection({
+      ...connectionOptions,
+      charset: 'UTF8MB4_GENERAL_CI',
+    } as MysqlConnectionOptions);
+  } catch (error) {
+    debug("application:database")(error);
+  }
+
   const app = Application.getInstance();
   SocketIOServer.start(app);
   app.start();
-}).catch(error => debug("application:database")(error));
+}
+
+init();
