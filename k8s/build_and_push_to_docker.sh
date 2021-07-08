27704bf4702b028be2d8ac26d411484a36a5768e
jessec1001 <jessecorralejr@gmail.com>
2021-07-08 05:55:44 +0300

    bumped socket.io version

--- k8s/build_and_push_to_docker.sh
+++ k8s/build_and_push_to_docker.sh
@@ -2,6 +2,7 @@ if [ -f ../.env ]
 then
   export $(cat ../.env | grep -v SECRET_KEY= | sed 's/#.*//g' | xargs)
 fi
+yarn install
 yarn run build
 docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
 docker build ../ -f ./containers/chat.Dockerfile -t "${DOCKER_HUB_USER}/${PROJECT_NAME}:chat_${DEPLOY_ENV}" && \
