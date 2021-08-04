f92bf19838477937e9b4c798ce38c7f191323da4
jessec1001 <jessecorralejr@gmail.com>
2021-08-04 08:46:55 +0300

    refactored code

--- src/repository/ConversationRepository.ts
+++ src/repository/ConversationRepository.ts
@@ -22,18 +22,7 @@ export default class ConversationRepository extends Repository<Conversation> {
   }
 
   async getConversations(id: string, skip = 0, take = 999999, since = 0): Promise<Conversation[]> {
-    let from = "1900-01-01";
-    if (since != 0){
-      var a = new Date(since * 1000);
-      var year = a.getFullYear();
-      var month = ("0" + (a.getMonth() + 1)).slice(-2)
-      var date = ("0" + a.getDate()).slice(-2)
-      var hour = a.getHours();
-      var min = a.getMinutes();
-      var sec = a.getSeconds();
-      from = year + '-' + month + '-' + date + ' ' + hour + ':' + min + ':' + sec ;
-    }console.log("HERE!", since, from)
-    let p = await this.createQueryBuilder("conversation")
+    return this.createQueryBuilder("conversation")
       .leftJoinAndSelect("conversation.participants", "participants")
       .leftJoinAndSelect(subQuery => {
         return subQuery
@@ -51,12 +40,10 @@ export default class ConversationRepository extends Repository<Conversation> {
       }, "conversationParticipants", '"conversationParticipants"."conversationId" = "conversation"."id"')
       .innerJoinAndMapOne("conversation.lastEvent", ChatEvent, 'events', '"events"."conversationId"="conversation"."id" AND "events"."createdAt" = "lastEvent"."createdAt"')
       .where('"conversationParticipants"."count" > 0')
-      .where('"conversation"."updatedAt" > :from',{from})
+      .where('"conversation"."updatedAt" > to_timestamp(:since)',{since})
       .take(take)
       .skip(skip)
       .orderBy("conversation.updatedAt", "DESC")
       .getMany();
-      
-      return p;
   }
 }
